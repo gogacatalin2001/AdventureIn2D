8a386369c31469416837d40da13282538de10e96
gogacatalin2001 <gogacatalin01@gmail.com>
2024-10-03 22:55:32 +0300

    Initial commit: main class, game panel, game loop and key handler

---
+++ src/main/java/GamePanel.java
@@ -0,0 +1,100 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class GamePanel extends JPanel implements Runnable {
+
+    // SCREEN SETTINGS
+    final int originalTileSize = 16; // 16x16 tile
+    final int scale = 3;
+
+    final int tileSize = originalTileSize * scale;
+    final int maxScreenCol = 16;
+    final int maxScreenRow = 12;
+    final int screenWidth = tileSize * maxScreenCol;
+    final int screenHeight = tileSize * maxScreenRow;
+
+    final int FPS = 60;
+
+    KeyHandler keyHandler = new KeyHandler();
+    Thread gameThread;
+
+    int playerX = 100;
+    int playerY = 100;
+    int playerSpeed = 4;
+
+    public GamePanel() {
+        this.setPreferredSize(new Dimension(screenWidth, screenHeight));
+        this.setBackground(Color.BLACK);
+        this.setDoubleBuffered(true);
+        this.addKeyListener(keyHandler);
+        this.setFocusable(true);
+    }
+
+    public void startGameThread() {
+        gameThread = new Thread(this);
+        gameThread.start();
+    }
+
+
+    @Override
+    public void run() {
+        double drawInterval = 1000000000 / FPS;
+        double delta = 0;
+        long lastTime = System.nanoTime();
+        long currentTime;
+        long timer = 0;
+        int drawCount = 0;
+
+        while(gameThread != null) {
+            currentTime = System.nanoTime();
+            delta += (currentTime - lastTime) / drawInterval;
+            timer += (currentTime - lastTime);
+            lastTime = currentTime;
+
+            if (delta >= 1) {
+                update();
+                repaint();
+                delta--;
+                drawCount++;
+            }
+
+            if (timer >= 1000000000) {
+                System.out.println("FPS: " + drawCount);
+                drawCount = 0;
+                timer = 0;
+            }
+        }
+    }
+
+    public void update() {
+        if (keyHandler.isDownPressed()) {
+            playerY += playerSpeed;
+        } else if (keyHandler.isUpPressed()) {
+            playerY -= playerSpeed;
+        } else if (keyHandler.isLeftPressed()) {
+            playerX -= playerSpeed;
+        } else if (keyHandler.isRightPressed()) {
+            playerX += playerSpeed;
+        }
+
+//        if (playerX <= 0)
+//            playerX = 0;
+//        if (playerX > screenWidth)
+//            playerX = screenWidth;
+//        if (playerY <= 0)
+//            playerY = 0;
+//        if (playerY > screenHeight)
+//            playerY = screenHeight;
+    }
+
+    @Override
+    public void paintComponent(Graphics g) {
+        Toolkit.getDefaultToolkit().sync();
+        super.paintComponent(g);
+
+        Graphics2D g2d = (Graphics2D) g;
+        g2d.setColor(Color.WHITE);
+        g2d.fillRect(playerX, playerY, tileSize, tileSize);
+        g2d.dispose();
+    }
+}
---
+++ src/main/java/KeyHandler.java
@@ -0,0 +1,63 @@
+import lombok.Getter;
+import lombok.Setter;
+
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+@Getter
+@Setter
+public class KeyHandler implements KeyListener {
+
+    private boolean upPressed;
+    private boolean downPressed;
+    private boolean leftPressed;
+    private boolean rightPressed;
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+        // not used
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        int code = e.getKeyCode();
+
+        switch (code) {
+            case KeyEvent.VK_W -> {
+                setUpPressed(true);
+            }
+            case KeyEvent.VK_A -> {
+                setLeftPressed(true);
+            }
+            case KeyEvent.VK_S -> {
+                setDownPressed(true);
+            }
+            case KeyEvent.VK_D -> {
+                setRightPressed(true);
+            }
+            default -> {}
+        }
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        int code = e.getKeyCode();
+
+        switch (code) {
+            case KeyEvent.VK_W -> {
+                setUpPressed(false);
+            }
+            case KeyEvent.VK_A -> {
+                setLeftPressed(false);
+            }
+            case KeyEvent.VK_S -> {
+                setDownPressed(false);
+            }
+            case KeyEvent.VK_D -> {
+                setRightPressed(false);
+            }
+            default -> {
+            }
+        }
+    }
+}
